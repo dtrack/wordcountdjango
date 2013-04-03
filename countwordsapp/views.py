3442993f266ff7e3717734648e85757a9f318950
Jeremy Henault <jhenault@saymedia.com>
2013-04-03 15:26:44 -0700

    ignore short words and some common words

--- countwordsapp/views.py
+++ countwordsapp/views.py
@@ -3,6 +3,10 @@ from operator import itemgetter
 from django.shortcuts import render
 
 
+ignored_words = set(
+    'the', 'is', 'was', 'a', 'i', 'to', 'in', 'of', 'and', 'you', 'your', 'we'
+)
+
 def home(req):
     context = {}
     if req.method == "POST":
@@ -17,9 +21,10 @@ def home(req):
 
         for w in re.findall(re.compile('[\w\d]+'), s.lower()):
             count_words += 1
-            if words.get(w) is None:
-                words[w] = 0
-            words[w] += 1
+            if w not in ignored_words or len(w) > 2:
+                if words.get(w) is None:
+                    words[w] = 0
+                words[w] += 1
         # sort word by counts
         sorted_words = sorted([
             {"word": word, "count": count} for word, count in words.iteritems()
