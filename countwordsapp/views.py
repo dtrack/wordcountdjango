2234f2e0e093e230dfaa1b65f1a55ac12707fc6e
Jeremy Henault <jhenault@saymedia.com>
2013-04-03 11:33:25 -0700

    count words from a text of file input and return nb of occurences of each word

---
+++ countwordsapp/views.py
@@ -0,0 +1,32 @@
+import re
+from operator import itemgetter
+from django.shortcuts import render
+
+
+def home(req):
+    context = {}
+    if req.method == "POST":
+        words = {}
+        count_words = 0
+        f = req.FILES.get('file-upload')
+        s = req.POST.get('text-field')
+        if f is not None:
+            s = f.read()
+        if not s:
+            context["error"] = "No text to analyze"
+
+        for w in re.findall(re.compile('[\w\d]+'), s.lower()):
+            count_words += 1
+            if words.get(w) is None:
+                words[w] = 0
+            words[w] += 1
+        # sort word by counts
+        sorted_words = sorted([
+            {"word": word, "count": count} for word, count in words.iteritems()
+        ], key=itemgetter('count'))
+        sorted_words.reverse()
+
+        context["words"] = sorted_words
+        context["count_words"] = count_words
+
+    return render(req, 'home.html', context)
